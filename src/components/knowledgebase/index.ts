fdd21f115f08a6ea8a4cdb0e973debfb037eef29
cbussalleu <100375866+cbussalleu@users.noreply.github.com>
2025-03-17 06:00:46 +0100

    Create index.ts

---
+++ src/components/knowledgebase/index.ts
@@ -0,0 +1,6 @@
+// This file exports all Knowledge Base components
+export * from './KBHero';
+export * from './KBLatestUpdates';
+export * from './KBResourcesSection';
+export * from './KBTopResourcesSection';
+export * from './KBHelpSection';
